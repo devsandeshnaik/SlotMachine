7303f0479daafb2f71622a776c6665bbf3d5a313
devsandeshnaik <devsandeshnaik@gmail.com>
2020-08-18 00:22:57 +0530

    Changes to color extension

--- Slot Machine/Helpers/Extension+Color.swift
+++ Slot Machine/Helpers/Extension+Color.swift
@@ -7,10 +7,15 @@
 //
 
 import SwiftUI
+import UIKit
 
 extension Color {
     static var colorPink: Color { Color("ColorPink") }
     static var colorPurple: Color { Color("ColorPurple") }
     static var colorYellow: Color { Color("ColorYellow") }
     static var colorTransparentBlack: Color { Color("ColorTransparentBlack") }
+}
+
+struct Labe: UILabel{
+    <#fields#>
 }
